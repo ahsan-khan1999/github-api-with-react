b3b10be2f597ae48b3e48315f2f73b1ac84b0f91
ahsan-khan1999 <ahsan.ak943@gmail.com>
2020-09-06 18:03:00 +0500

    added uuid module for uinque id generation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
-
+import { v4 as uuidV4} from 'uuid'
 import './App.css';
 
 function App() {
@@ -19,12 +19,13 @@ function App() {
   // ,[]} in earlier react we have commingprops and rendered props if these are not equal then only we need to call api now 
   // we just leave these [] as it is if we need to call for the first time only once
   // now if we recieve an event and we dont want to reflect the effect of this hook to dom then we need to maintain an identifier here and we will toggle that identifer when we recieve an event related to it 
+  // useEffect(callback[, deps]) hook. This hook executes callback after mounting, and after subsequent renderings when deps change
 
   return (
     <div className="App">
       <ul>{
-        repos.map((d, ind) => {
-          return <li key={ind}>{d.name}</li>
+        repos.map((d) => {
+          return <li key={uuidV4()}>{d.name}</li>
         })}
       </ul>
 
